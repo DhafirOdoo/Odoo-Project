0ad6678858c80cf2c4ac390b22d2c553fac0cb5e
NihalAhamedK <nihalahamedva492@gmail.com>
2026-02-13 11:40:52 +0530

    author details add

--- default_receivable_account/__manifest__.py
+++ default_receivable_account/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'Default Receivable Account',
     'depends': ['base','account','base_accounting_kit','base_account_budget'],
     'summary': 'Default Receivable Account for Partners',
+    'author' : 'Dhafir Technologies',
     'installable': True,
     'application': True,
     'data': [
