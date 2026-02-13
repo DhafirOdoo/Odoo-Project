32e5345812c13aff1846d8087f4784425a84615e
NihalAhamedK <nihalahamedva492@gmail.com>
2026-02-13 10:53:03 +0530

    manifest summary change

--- general_ledger_account_filter/__manifest__.py
+++ general_ledger_account_filter/__manifest__.py
@@ -1,10 +1,9 @@
 {
     'name': 'General Ledger Account Filter',
     'depends': ['account','base','base_accounting_kit'],
-    'summary': 'Account Filter for General Ledger Report',
+    'summary': 'Enable Account Filter for General Ledger Report',
     'installable': True,
     'application': True,
-    'summary': 'Enable General Ledger Account Filter',
     'data': [
         'views/general_ledger_account_filter_views.xml',
     ]
