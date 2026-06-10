fdac344d1e7a7c9583e79b62d7d9ef6bd5a0fe3b
Nihal Ahamed <nihalahamedva492@gmail.com>
2026-06-10 10:41:07 +0530

    hostel payment structure

---
+++ hostel_payment_structure_custom/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Hostel Payment Structure Custom',
+    'version': '17.0.0.1.0',
+    'depends': ['dev_hostel_management'],
+    'author': 'Dhaifr Technologies',
+    'installable': True,
+    'data':[
+        'views/hostel_payment_structure_view_inherit.xml',
+    ]
+}
