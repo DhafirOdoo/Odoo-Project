f43523b4ff89806944047c6e4517200c363b7b63
Nihal Ahamed <nihalahamedva492@gmail.com>
2026-07-16 11:18:20 +0400

    leave allocation request mail

--- timeoff_mail_template/__manifest__.py
+++ timeoff_mail_template/__manifest__.py
@@ -7,7 +7,9 @@
     'summary': 'Custom Mail Template For Leave Request E-mail',
     'data': [
         'data/mail_template_data.xml',
+        'data/allocation_mail_template_data.xml',
         'views/timeoff_form_view_inherit.xml',
+        'views/hr_leave_allocation_form.xml',
     ],
 
 }
